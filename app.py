3795003f9db4fbe98a92944c836dfa61a60f3751
Paul Borawski <paulborawski14@gmail.com>
2021-09-25 11:34:24 -0700

    try number two

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS, cross_origin
 from flask import jsonify
 import base64
 import hashlib
+from Cryptodome.Util.Padding import pad, unpad
 from Cryptodome import Random
 from Cryptodome.Cipher import AES
 
@@ -14,15 +15,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/user-info', methods = ['GET'])
 def main():
-    key = '1234567890123456'
-    def decrypt(enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
     user_string = request.args.get('user_string')
-    result = decrypt(user_string)
-    print(rersult)
+    key = b'1234567890123456'
+    enc = base64.b64decode(user_string)
+    iv = enc[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    username = cipher.decrypt(user_string, 'utf-8')
+    print(len(username))
+    user_string = request.args.get('user_string')
+
     return result
 
 if __name__ == "__main__":
---
+++ client/src/components/InputTryTwo.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import { Buffer } from 'buffer'
+
+function InputTryTwo() {
+  const sendData = () => {
+
+    var aes256 = {},
+        crypto = require('crypto'),
+        algorithm = 'aes-256-cbc';
+
+    aes256.encrypt = function (key, data) {
+        var sha256 = crypto.createHash('sha256');
+        sha256.update(key);
+
+        var iv = crypto.randomBytes(16),
+            plaintext = new Buffer(data),
+            cipher = crypto.createCipheriv(algorithm, sha256.digest(), iv),
+            ciphertext = cipher.update(plaintext);
+        ciphertext = Buffer.concat([iv, ciphertext, cipher.final()]);
+
+        return ciphertext.toString('base64');
+  };
+  let user_key = '1234567890123456'
+  let user_data = 'username'
+  let encrypted_string = aes256.encrypt(user_key, user_data)
+  console.log(encrypted_string)
+    axios.get('http://localhost:4444/user-info', {
+      params: { user_string: encrypted_string }
+    })
+      .then(res => {
+        console.log(res)
+      })
+  }
+  return (
+    <div>
+      <button onClick={sendData}>click me</button>
+    </div>
+  )
+}
+
+export default InputTryTwo
