3f1c1142b6a2e81c53ddf25787f05163abe4f1e6
Paul Borawski <paulborawski14@gmail.com>
2021-09-25 10:45:19 -0700

    saving this state of the app

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 from flask import jsonify
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -9,7 +14,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/user-info', methods = ['GET'])
 def main():
-    return 'user info working'
+    key = '12345678'
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+    user_string = request.args.get('user_string')
+    result = decrypt(user_string)
+    print(rersult)
+    return result
 
 if __name__ == "__main__":
     app.run(port=4444, debug=True)
--- client/src/components/InputComponent.jsx
+++ client/src/components/InputComponent.jsx
@@ -1,8 +1,32 @@
 import axios from 'axios'
+import { Buffer } from 'buffer'
 
 function InputComponent() {
   const sendData = () => {
-    axios.get('http://localhost:4444/user-info')
+
+    var aes256 = {},
+        crypto = require('crypto'),
+        algorithm = 'aes-256-cbc';
+
+    aes256.encrypt = function (key, data) {
+        var sha256 = crypto.createHash('sha256');
+        sha256.update(key);
+
+        var iv = crypto.randomBytes(16),
+            plaintext = new Buffer(data),
+            cipher = crypto.createCipheriv(algorithm, sha256.digest(), iv),
+            ciphertext = cipher.update(plaintext);
+        ciphertext = Buffer.concat([iv, ciphertext, cipher.final()]);
+
+        return ciphertext.toString('base64');
+  };
+  let user_key = '12345678'
+  let user_data = 'username'
+  let encrypted_string = aes256.encrypt(user_key, user_data)
+  console.log(encrypted_string)
+    axios.get('http://localhost:4444/user-info', {
+      params: { user_string: encrypted_string }
+    })
       .then(res => {
         console.log(res)
       })
